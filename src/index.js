71827dadd3aa4cf62fde49e32c71936eb0f4840f
Cosir1 <72754823+Cosir1@users.noreply.github.com>
2025-02-10 13:01:11 +0300

    Key

--- src/index.js
+++ src/index.js
@@ -4,38 +4,43 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.get("/api/youtube/:channelId", async (req, res) => {
-  const results = [];
-  let pageToken = null;
-  const baseURL = "https://www.googleapis.com/youtube/v3/search";
+app.get(
+  "/api/youtube/:channelId/:publishedBefore/:publishedAfter/:key",
+  async (req, res) => {
+    const results = [];
+    let pageToken = null;
+    const baseURL = "https://www.googleapis.com/youtube/v3/search";
 
-  try {
-    do {
-      const response = await axios.get(baseURL, {
-        params: {
-          part: "snippet",
-          channelId: req.params.channelId,
-          maxResults: 50,
-          type: "video",
-          key: "AIzaSyAbEgk0HUGGR5QRN-oGTnDbktemUu_DEX0",
-          pageToken: pageToken,
-        },
-      });
+    try {
+      do {
+        const response = await axios.get(baseURL, {
+          params: {
+            part: "snippet",
+            channelId: req.params.channelId,
+            maxResults: 50,
+            type: "video",
+            key: req.params.key,
+            pageToken: pageToken,
+            publishedAfter: req.params.publishedAfter,
+            publishedBefore: req.params.publishedBefore,
+          },
+        });
 
-      results.push(...response.data.items);
-      pageToken = response.data.nextPageToken || null;
-    } while (pageToken);
+        results.push(...response.data.items);
+        pageToken = response.data.nextPageToken || null;
+      } while (pageToken);
 
-    res.json(results);
-  } catch (error) {
-    if (error.response) {
-      throw new Error(
-        `YouTube API Error: ${error.response.data.error.message}`
-      );
+      res.json(results);
+    } catch (error) {
+      if (error.response) {
+        throw new Error(
+          `YouTube API Error: ${error.response.data.error.message}`
+        );
+      }
+      throw error;
     }
-    throw error;
   }
-});
+);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
