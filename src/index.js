914b0d3067f6584483477b79a981b054c1c8925c
Cosir1 <72754823+Cosir1@users.noreply.github.com>
2025-02-10 14:22:22 +0300

    Update 2

--- src/index.js
+++ src/index.js
@@ -4,43 +4,42 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.get(
-  "/api/youtube/:channelId/:publishedBefore/:publishedAfter/:key",
-  async (req, res) => {
-    const results = [];
-    let pageToken = null;
-    const baseURL = "https://www.googleapis.com/youtube/v3/search";
+app.get("/api/youtube", async (req, res) => {
+  const results = [];
+  let pageToken = null;
+  const baseURL = "https://www.googleapis.com/youtube/v3/search";
+  const { channelId, publishedBefore, publishedAfter, key } = req.query;
 
-    try {
-      do {
-        const response = await axios.get(baseURL, {
-          params: {
-            part: "snippet",
-            channelId: req.params.channelId,
-            maxResults: 50,
-            type: "video",
-            key: req.params.key,
-            pageToken: pageToken,
-            publishedAfter: req.params.publishedAfter,
-            publishedBefore: req.params.publishedBefore,
-          },
-        });
+  console.log(req.query);
+  try {
+    do {
+      const response = await axios.get(baseURL, {
+        params: {
+          part: "snippet",
+          channelId: channelId,
+          maxResults: 50,
+          type: "video",
+          key: key,
+          pageToken: pageToken,
+          publishedAfter: publishedAfter,
+          publishedBefore: publishedBefore,
+        },
+      });
 
-        results.push(...response.data.items);
-        pageToken = response.data.nextPageToken || null;
-      } while (pageToken);
+      results.push(...response.data.items);
+      pageToken = response.data.nextPageToken || null;
+    } while (pageToken);
 
-      res.json(results);
-    } catch (error) {
-      if (error.response) {
-        throw new Error(
-          `YouTube API Error: ${error.response.data.error.message}`
-        );
-      }
-      throw error;
+    res.json(results);
+  } catch (error) {
+    if (error.response) {
+      throw new Error(
+        `YouTube API Error: ${error.response.data.error.message}`
+      );
     }
+    throw error;
   }
-);
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
