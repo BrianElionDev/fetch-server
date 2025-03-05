3d63dc7da5cd801b2b2f3bc976f00fc2fbab4c6b
Cosir1 <72754823+Cosir1@users.noreply.github.com>
2025-03-05 15:54:50 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -297,6 +297,6 @@ app.get("/api/pubsub/subscribe", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3000, () => console.log("Server running on 3000."));
 
 export default app;
