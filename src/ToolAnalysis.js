704cba7172a90e8b1a82e3ff4b8c8ab7837da773
Ash <briankosir@gmail.com>
2025-03-26 17:48:04 +0300

    Up

--- src/ToolAnalysis.js
+++ src/ToolAnalysis.js
@@ -2,9 +2,7 @@ import { fetchTranscript, fetchTranscriptFromAPI } from "./FetchTranscript.js";
 import { makeLlmPrompt } from "./Llm.js";
 
 export const makeAnalysis = async ({ url, model }) => {
-  console.log("URL", url);
   const transcript = await fetchTranscriptFromAPI(url);
-  console.log("Transcript", transcript);
   const { analysis, summary } = await makeLlmPrompt({
     transcript: transcript?.content,
     model: model,
